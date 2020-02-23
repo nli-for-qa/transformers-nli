8246aa4a37bc6cf71d7414991e161b3dadb88f3a
Anshuman Mishra <anshuman@Anshumans-MacBook-Pro.local>
2020-02-23 15:03:30 -0500

    (bug) Adding nli imports to init

--- src/transformers/data/processors/__init__.py
+++ src/transformers/data/processors/__init__.py
@@ -6,3 +6,4 @@ from .glue import glue_convert_examples_to_features, glue_output_modes, glue_pro
 from .squad import SquadExample, SquadFeatures, SquadV1Processor, SquadV2Processor, squad_convert_examples_to_features
 from .utils import DataProcessor, InputExample, InputFeatures, SingleSentenceClassificationProcessor
 from .xnli import xnli_output_modes, xnli_processors, xnli_tasks_num_labels
+from .nli import nli_convert_examples_to_features, nli_output_modes, nli_processors, nli_task_num_labels
